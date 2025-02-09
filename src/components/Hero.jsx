be5412ef2ba795899477fa2ded43cd3ab33b52d8
urmilachavan27 <131237674+urmilachavan27@users.noreply.github.com>
2025-02-09 22:40:30 +0530

    hero section in first section running

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,12 +3,13 @@ import TruckImage from "../images/hero/truck.svg";
 import WheelImage1 from "../images/hero/tyre1.svg";
 import WheelImage2 from "../images/hero/tyre2.svg";
 import { Link } from "react-router-dom";
-
+import { FaArrowRight } from "react-icons/fa";
+import Dashboard from '../images/hero/Dashboard.webp'
 const Hero = () => {
   return (
     <>
 
-      <section className="campaign-section container py-5">
+      {/* <section className="campaign-section container py-5">
         <div className="row align-items-center">
           <div className="col-md-6">
             <h2 className="fw-bold">Automatic e-way bill</h2>
@@ -43,15 +44,30 @@ const Hero = () => {
           </div>
         </div>
 
-        {/* Road Container */}
 
-      </section>
-        <div className="road-container ">
+      </section> */}
+      {/* <div className="road-container ">
         <div className="road-img">
           
           </div>
+        </div> */}
+      <section className="accountingsoftware-main">
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-md-6">
+              <h1 className="accountingsoftware-heading">#1 Free Accounting Software <span>For <br /> Small Business Owners</span></h1>
+              <p className="accountingsoftware-p">ProfitBooks simplifies business accounting, invoicing, inventory tracking <br />and reporting using one powerful interface.</p>
+              <p className="accountingsoftware-p">100% FREE accounting software. No strings attached.</p>
+              <button className="accountingsoftware-btn">Create Your FREE Account Now <span className="accountingsoftware-icon"><FaArrowRight /></span></button>
+            </div>
+            <div className="col-md-6">
+              <img src={Dashboard} width="100%" height="100%" />
+            </div>
+
+          </div>
         </div>
-   
+      </section>
+
     </>
   );
 };
